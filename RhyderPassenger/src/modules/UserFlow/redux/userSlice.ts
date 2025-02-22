2105186307f50e53c6428d496ada68a0a0aad79b
Niralak025 <niralak025@gmail.com>
2025-02-22 18:52:47 +0530

    Added profile api call + tab navigator

--- RhyderPassenger/src/modules/UserFlow/redux/userSlice.ts
+++ RhyderPassenger/src/modules/UserFlow/redux/userSlice.ts
@@ -1,29 +1,47 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 interface UserState {
   firstName: string;
   lastName: string;
   email: string;
   profilePic: string | null;
+  loading: boolean;
+  loaded: boolean;
+  error: Error | undefined | string;
 }
 
 const initialState: UserState = {
-  firstName: "",
-  lastName: "",
-  email: "",
+  firstName: '',
+  lastName: '',
+  email: '',
   profilePic: null,
+  loading: false,
+  loaded: false,
+  error: undefined,
 };
 
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {
     setUser: (state, action: PayloadAction<Partial<UserState>>) => {
-      return { ...state, ...action.payload };
+      return {...state, ...action.payload};
     },
     clearUser: () => initialState,
+    userLoading: state => {
+      (state.loading = true), (state.loaded = false), (state.error = undefined);
+    },
+    userLoaded: state => {
+      (state.loading = false), (state.loaded = true);
+    },
+    userError: (state, action: PayloadAction<Error | string>) => {
+      (state.loading = false),
+        (state.loaded = true),
+        (state.error = action.payload);
+    },
   },
 });
 
-export const { setUser, clearUser } = userSlice.actions;
+export const {setUser, clearUser, userLoading, userLoaded, userError} =
+  userSlice.actions;
 export default userSlice.reducer;
