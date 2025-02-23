d4d46fae35a0b9d5bc0ea00afe03ad3a14399cc0
AbhishekGupta-33 <gupta.abhi0003@gmail.com>
2025-02-23 19:37:42 +0530

    Added google JSON

--- RhyderPassenger/ios/Rhyder/AppDelegate.mm
+++ RhyderPassenger/ios/Rhyder/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>  // Import Firebase
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
   self.moduleName = @"Rhyder";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
